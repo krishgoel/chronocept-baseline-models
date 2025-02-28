c5c7ed2ead41b841b08ee1ec16521d26aa860e98
sanskar240 <71585678+sanskar240@users.noreply.github.com>
2025-02-28 21:55:14 +0530

    add: SVR architecture

--- models/__init__.py
+++ models/__init__.py
@@ -3,3 +3,4 @@ from .ffnn import FFNNModel
 from .bilstm import BiLSTMModel
 from .xgboost import XGBoostRegressionModel
 from .linear_regression import LinearRegressionModel
+from .svr import SVRModel
